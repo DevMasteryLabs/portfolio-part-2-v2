1acd1beb53c21c259f6869f66fc56afa9065ccd1
DevMasteryLabs <develop.devmastery@gmail.com>
2023-09-22 00:14:44 +0200

    Initial commit (Starter code from part 1)

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Navbar from "./components/Navbar"
+import Home from './pages/Home'
+import Skills from './pages/Skills'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/skills' element={<Skills />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Portfolio</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <NavLink to='/' className='nav-link'>Home</NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink to='/skills' className='nav-link'>Skills</NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink to='/projects' className='nav-link'>Projects</NavLink>
+            </li>
+            <li class="nav-item">
+              <NavLink to='/contact' className='nav-link'>Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
