3bad6410c612793cae5cf993ca329070de7fb896
DevMasteryLabs <develop.devmastery@gmail.com>
2023-09-22 01:01:19 +0200

    Set up the Skills page using 'axios'

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Skills() {
+
+  const [technologies, setTechnologies] = useState([])
+
+  useEffect(() => {
+    axios.get('https://devmastery-assets.vercel.app/technologies.json')
+      .then((result) => {
+        setTechnologies(result.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
   return (
-    <div>Skills</div>
+    <div>
+      <div className='row'>
+        {technologies.map((tech) => {
+          return (
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3 p-3" key={tech.id}>
+              <div className="card p-3">
+                <img src={tech.image} className="card-img-top" alt={tech.name} />
+                <div className="card-body">
+                  <h5 className="card-title">{tech.name}</h5>
+                  <a href="#" className="btn btn-primary">Learn about</a>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
