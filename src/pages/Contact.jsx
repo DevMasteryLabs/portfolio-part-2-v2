4814ae1885fdd0fda57a358daf8b88f4332db1bb
DevMasteryLabs <develop.devmastery@gmail.com>
2023-09-22 01:35:57 +0200

    Set up the Contact page using "bootstrap-icons"

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,37 @@
 
+const contactInformations = {
+  email: 'jane@gmail.com',
+  phone: '+216 50 123 456',
+  address: {
+    street: 'Avenue de Cathage',
+    city: 'Sfax',
+    postalCode: '3027',
+    country: 'Tunisia'
+  }
+}
+
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <div>
+        <i className="bi bi-envelope"></i>
+        <span className="ms-2">
+          {contactInformations.email}
+        </span>
+      </div>
+      <div className='mt-3'>
+        <i className="bi bi-phone-fill"></i>
+        <span className="ms-2">
+          {contactInformations.phone}
+        </span>
+      </div>
+      <div className='mt-3'>
+        <i className="bi bi-geo-alt-fill"></i>
+        <span className="ms-2">
+          {contactInformations.address.street}, {contactInformations.address.city}, {contactInformations.address.postalCode}, {contactInformations.address.country}
+        </span>
+      </div>
+    </div>
   )
 }
 
